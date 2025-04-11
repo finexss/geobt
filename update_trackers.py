8a540051ad42c45d3290c10c517fa26697746395
new <154498355+finexss@users.noreply.github.com>
2025-04-12 02:45:31 +0800

    Update update_trackers.py

--- update_trackers.py
+++ update_trackers.py
@@ -123,6 +123,7 @@ def main():
 
     tracker_list_urls = [
         "https://api.yaozuopan.top:88/composite?key=bt&auth=3cae9a3a53f1daef137126648a535ab7",
+        "https://www.gbsat.org/bt/tracker.txt",
         "https://github.com/XIU2/TrackersListCollection/raw/master/all.txt",
         "https://raw.githubusercontent.com/Tunglies/TrackersList/refs/heads/main/all.txt",
         "https://github.com/XIU2/TrackersListCollection/raw/master/all_aria2.txt",
